3e3e02da0e34ab694c9d0c2b6944cc941a236f3a
Fernando Fontes <fernando.fontes@gomind.com.br>
2024-08-30 09:49:41 -0300

    feat: adding summary to the methods

--- lib/ezNavigator/webdriver_manager.py
+++ lib/ezNavigator/webdriver_manager.py
@@ -25,10 +25,16 @@ class Manager:
     performance_logs_mode = False
     browser_logs_mode = False
     
-    def add_options(self) -> uc.ChromeOptions:   
+    def add_options(self) -> uc.ChromeOptions: 
+        """
+        Returns ChromeOptions to configure the Chrome driver before instantiation.
+        """  
         return uc.ChromeOptions()
     
-    def get_driver(self, driverexe_path: Union[Path, str], chrome_options: uc.ChromeOptions, browser_logs: bool = False, performance_logs: bool = False) -> Optional[WebDriver]:
+    def get_driver(self, driverexe_path: Union[Path, str], chrome_options: uc.ChromeOptions, browser_logs: bool = False, performance_logs: bool = False) -> Optional[WebDriver]:   
+        """
+        Instantiates and returns a WebDriver object with optional logging capabilities.
+        """
         driverexe_path = str(driverexe_path)
         
         if os.path.isfile(driverexe_path):
@@ -48,6 +54,9 @@ class Manager:
             return uc.Chrome(chrome_options, driver_executable_path = driverexe_path)
     
     def search_by_element(self, driver: WebDriver, by: Literal['id', 'name', 'xpath', 'link_text', 'partial_link_text', 'tag_name', 'class_name', 'css_selector'], param: str, timeout: int = 10) -> Optional[WebElement]:
+        """
+        Waits for and returns a WebElement located by a specified method (e.g., ID, XPath).
+        """
         by_mapping = {
             "id"               : By.ID,
             "name"             : By.NAME,
@@ -73,6 +82,9 @@ class Manager:
         return element
 
     def search_by_element_or_null(self, driver: WebDriver, by: Literal['id', 'name', 'xpath', 'link_text', 'partial_link_text', 'tag_name', 'class_name', 'css_selector'], param: str, timeout: int = 10) -> Optional[WebElement]:
+        """
+        Attempts to find a WebElement. Returns None if element is not found within the specified timeout.
+        """
         try:
             element = self.search_by_element(driver, by, param, timeout)
             return element
@@ -81,6 +93,9 @@ class Manager:
             return None
         
     def change_iframe(self, driver: WebDriver, by: Optional[Literal['id', 'name', 'xpath', 'link_text', 'partial_link_text', 'tag_name', 'class_name', 'css_selector']] = None, param: Optional[str] = None) -> None:
+        """
+        Switches the WebDriver context to an iframe or back to the main content.
+        """
         if by is None or param is None:
             driver.switch_to.default_content()
             return
@@ -89,6 +104,9 @@ class Manager:
         driver.switch_to.frame(frame_element)
     
     def get_all_headers(self, driver: WebDriver) -> Optional[List[Dict[str, str]]]:
+        """
+        Retrieves all headers from network logs recorded during the browser session.
+        """
         if self.performance_logs_mode == False:
             raise RuntimeError("Driver was not configured to record logs")
 
@@ -115,6 +133,9 @@ class Manager:
         return list_headers
 
     def search_by_image_or_null(self, image_paths: Union[str, List[str], Path, List[Path]], search_time: int, region: Optional[Tuple[int, int, int, int]] = None, confidence: float = 0.7, grayscale: bool = False) -> Optional[pyautogui.Point]:
+        """
+        Searches for an image on the screen within a specified time and returns its center coordinates if found.
+        """
         if isinstance(image_paths, (str, Path)):
             image_paths = [Path(image_paths).as_posix()]
             
@@ -145,6 +166,9 @@ class Manager:
         return None
     
     def capture_screenshot(self, driver: WebDriver, filename: str, download_path: Optional[str] = None, region: Optional[Tuple[int, int, int, int]] = None) -> None:
+        """ 
+        Captures a screenshot of the browser and saves it to the specified location.
+        """
         screenshot = driver.get_screenshot_as_png()
         
         if region:
@@ -165,15 +189,24 @@ class Manager:
         screenshot_image.save(download_path, format='PNG')
     
     def scroll_page(self, driver: WebDriver, direction: Literal['up', 'down'] = 'down', amount: int = 300) -> None:
+        """
+        Scrolls the page in the specified direction by a given amount.
+        """
         if direction == 'down':
             driver.execute_script(f"window.scrollBy(0, {amount});")
         elif direction == 'up':
             driver.execute_script(f"window.scrollBy(0, -{amount});")
     
     def execute_script(self, driver: WebDriver, script: str, *args) -> any:
+        """
+        Executes a JavaScript script in the context of the browser.
+        """
         return driver.execute_script(script, *args)
 
     def get_performance_logs(self, driver: WebDriver) -> List[Dict[str, str]]:
+        """
+        Retrieves all performance logs generated during the browser session.
+        """
         if self.performance_logs_mode == False:
             raise RuntimeError("Driver was not configured to record console logs")
 
@@ -185,6 +218,9 @@ class Manager:
             return []
     
     def get_console_logs(self, driver: WebDriver) -> List[Dict[str, str]]:
+        """
+        Retrieves all console logs generated by the browser.
+        """
         if self.browser_logs_mode == False:
             raise RuntimeError("Driver was not configured to record console logs")
 
@@ -196,24 +232,39 @@ class Manager:
             return []
 
     def get_local_storage(self, driver: WebDriver) -> dict:
+        """
+        Returns the contents of the browser's local storage.
+        """
         return driver.execute_script("return window.localStorage;")
 
     def set_local_storage(self, driver: WebDriver, key: str, value: str) -> None:
+        """
+        Sets a key-value pair in the browser's local storage.
+        """
         driver.execute_script(f"window.localStorage.setItem('{key}', '{value}');")
     
     def accept_alert(self, driver: WebDriver) -> None:
+        """
+        Accepts a browser alert if one is present.
+        """
         try:
             WebDriverWait(driver, 10).until(expected_conditions.alert_is_present()).accept()
         except TimeoutException:
             print("No alert present")
 
     def dismiss_alert(self, driver: WebDriver) -> None:
+        """
+        Dismisses a browser alert if one is present.
+        """
         try:
             WebDriverWait(driver, 10).until(expected_conditions.alert_is_present()).dismiss()
         except TimeoutException:
             print("No alert present")
 
     def navigate_and_interact(self, image: Union[str, Path, pyautogui.Point], action: Literal['click', 'close', 'double_click'] = 'click', search_time: int = 10) -> bool:
+        """
+        Finds an image on the screen and performs an action on it (click, close, or double-click).
+        """
         if isinstance(image, (str, Path)):
             location = self.search_by_image_or_null(image, search_time)
             
@@ -231,6 +282,9 @@ class Manager:
         return False
 
     def center_mouse_and_click(self, click_mouse: Optional[bool] = True) -> None:
+        """
+        Moves the mouse's cursor to the center of the screen
+        """
         screen_width, screen_height = pyautogui.size()
         
         center_x = screen_width // 2
